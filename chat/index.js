84c72d1e968dcf2ddb1ee721f31a77cdc227548c
Antti <antti.vas@gmail.com>
2019-10-23 16:12:59 +0000

    pari typoo

--- chat/index.js
+++ chat/index.js
@@ -1,12 +1,24 @@
+ 
+var hostname = "127.0.0.1:5678"
 
 var types = require('./testprotocol_pb');
-var ws = new WebSocket("ws://127.0.0.1:5678/");
+var ws = new WebSocket(hostname);
 ws.binaryType = 'arraybuffer';
 
 var messages = document.createElement('ul');
 ws.binaryType = 'arraybuffer';
-document.getElementById('sendbutton').onclick = textboxClick;
+
+var sendbutton = document.getElementById('sendbutton');
+sendbutton.onclick = textboxClick;
+
 var textbox = document.getElementById('textbox');
+textbox.addEventListener("keyup", function(evnt) {
+	evnt.preventDefault();
+	if (evnt.keyCode === 13) { 
+		sendbutton.click(); 
+	}
+	return false;
+});
 
 ws.onmessage = function (event) {
     console.debug("WebSocket message received:", event);
@@ -17,9 +29,10 @@ ws.onmessage = function (event) {
     messages.appendChild(message);
 };
 
-function textboxClick() {
+function textboxClick(evnt) {
     //var bytes = proto.testi.Chatmessage.prototype.serializeBinary(textbox.value);
     var bytes = textbox.value;
     ws.send(bytes);
+	evnt.preventDefault();
 }
 document.body.appendChild(messages);
--- server/server.py
+++ server/server.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 
-# WS server that sends messages at random intervals
-
 import asyncio
 import testprotocol_pb2
-import datetime
-import random
 import websockets
+import html
+import socket
+import os
+
 
 clients = set()
-lock = asyncio.Lock()
+
+hostname = "127.0.0.1"
+port = 5678;
 
 def counter():
     counter.count += 1
     return counter.count - 1
 counter.count = 0
 
-async def adduser(websocket):
+def adduser(websocket):
     clients.add(websocket)
 
-async def removeuser(websocket):
+def removeuser(websocket):
     clients.remove(websocket)
 
 async def sendmessage(websocket, msg):
@@ -27,21 +29,27 @@ async def sendmessage(websocket, msg):
         await asyncio.wait([client.send(msg) for client in clients]) #if client != websocket])
 
 async def serv(websocket, path):
+    print("New connection!")
     msg = testprotocol_pb2.Chatmessage()
     msg.senderID = counter()
     msg.senderName = "testi"
-    await adduser(websocket)
-    try:
-        while True:
+    adduser(websocket)
+    while True:
+        try:
             async for message in websocket:
                 print(message)
-                msg.msg = message
+                msg.msg = html.escape(message)
                 bytes = msg.SerializeToString()
                 await sendmessage(websocket, bytes)
-    finally:
-        await removeuser(websocket)
+        except:
+            print("except")
+        finally:
+            removeuser(websocket)
+            break
 
-start_server = websockets.serve(serv, "127.0.0.1", 5678)
+start_server = websockets.serve(serv, hostname, port)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
+
