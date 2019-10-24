7989b63eb3f54f44887d7012bf2cd44abd2722c2
Ville Paju <vvpaju@github.com>
2019-10-24 16:27:55 +0300

    Revert "Toiston poistoa"
    This reverts commit 9f7d1a05cd56763ed4d510f5e2b24febcd9667d4.

--- kartta/index.js
+++ kartta/index.js
@@ -25,6 +25,50 @@ var myPosition = transform([25.749498121, 62.241677684], "EPSG:4326", "EPSG:3067
 var parser = new WMTSCapabilities();
 var map;
 
+
+var types = require('./testprotocol_pb');
+class WSHandler {
+	constructor(hostname) {
+		this.ws = new WebSocket(hostname);
+		this.ws.binaryType = "arraybuffer";
+		this.onChatMessage = new Set();
+		this.onLocationChange = new Set();
+		var me = this;
+		
+		this.ws.onmessage = function(evnt) {
+			me.onMessage(evnt)};
+	}
+
+	sendChatMessage(msg) {
+		this.ws.send(msg);
+	}
+	onMessage(evnt) {
+		var bytes = proto.testi.Chatmessage.deserializeBinary(evnt.data);
+
+		this.onChatMessage.forEach(function(arg) { arg(bytes)});
+	}
+
+	addChatMessageListener(func) {
+		this.onChatMessage.add(func);
+	}
+	removeChatMessageListener(func) {
+		this.onChatMessage.delete(func);
+	}
+
+}
+var wsh = new WSHandler("ws://127.0.0.1:5678");
+
+// esim. chat eventtien lukeminen
+function test(msg) {
+		var messages = document.getElementById('chattesti');
+		var message = document.createElement('li');
+		message.appendChild(document.createTextNode(msg.getMsg()));
+		messages.appendChild(message);
+}
+wsh.addChatMessageListener(test);
+//wsh.removeChatMessageListener(test);
+// end
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         var latitude = position.coords.latitude;
@@ -32,10 +76,8 @@ if (navigator.geolocation) {
         var accuracy = position.coords.accuracy;
         var debuginfo = document.getElementById("debuginfo");
         debuginfo.innerHTML = "latitude: " + latitude + ", longitude: " + longitude + ", accuracy: " + accuracy;
-		
-		myPosition = transform([longitude, latitude], "EPSG:4326", "EPSG:3067");
-		
-	});
+        myPosition = transform([longitude, latitude], "EPSG:4326", "EPSG:3067");
+  });
 } else {
     alert("Geolocation API is not supported in your browser.");
 }
@@ -65,7 +107,8 @@ fetch(capabilitiesUrl).then(function(response) {
 			})
 		});
 		});
-
+    
+    
 // https://openlayers.org/en/latest/examples/mouse-position.html
 var mousePositionControl = new MousePosition({
   coordinateFormat: createStringXY(4),
@@ -86,3 +129,4 @@ precisionInput.addEventListener('change', function(event) {
   var format = createStringXY(event.target.valueAsNumber);
   mousePositionControl.setCoordinateFormat(format);
 });
+
