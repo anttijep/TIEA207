e7e7757f82b4cf74c237d083971f04ee1f8fafca
localadmin <localadmin@sovproj09.ad.jyu.fi>
2019-11-22 14:45:12 +0200

    a

--- kartta/index.js
+++ kartta/index.js
@@ -204,7 +204,6 @@ wsh.addChatMessageListener(test);
 
 function updateLocation(msg) {
 	if (msg.getSenderid() === myId) return;
-	console.log(msg.getSenderid());
 	var s = msg.getSenderid() + ": " + msg.getLatitude()+ ", " + msg.getLongitude();
 	var lonlat = transform([msg.getLongitude(), msg.getLatitude()], "EPSG:4326", "EPSG:3067");
 	if (msg.getSenderid() in markerDict) {
@@ -219,7 +218,7 @@ function updateLocation(msg) {
 				}),
 				stroke: new Stroke({
 					color: '#000000',
-					width: 2
+					width: 3
 				})
 			})	
 		}));
